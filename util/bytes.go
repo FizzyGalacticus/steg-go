a544deda84eb3b4bf078764c0a520c75cc20c695
Dustin L. Dodson <dustinleedodson@gmail.com>
2019-03-28 19:02:11 -0700

    Added more util fns for byte/bit manipulation.

--- util/bytes.go
+++ util/bytes.go
@@ -16,12 +16,38 @@ func GetBitsFromByte(b byte) []bool {
 	bits := make([]bool, GetNumBitsFromBytes(&[]byte{b}))
 
 	for i := 1; i < 9; i++ {
-		val := int(b) & (1 << uint(8-i))
+		val := int8(b) & (1 << uint8(8-i))
 
 		bits[i-1] = val > 0
 	}
 
 	return bits
+}
+
+// GetByteFromBits - Converts 8-bit array to
+// a byte
+func GetByteFromBits(bits []bool) byte {
+	var b int8
+
+	for i, bit := range bits {
+		if bit {
+			b = b | (1 << uint8(7-i))
+		}
+	}
+
+	return byte(b)
+}
+
+// GetBytesFromBits - Converts a bit array to
+// a byte array
+func GetBytesFromBits(bits []bool) []byte {
+	bytes := make([]byte, len(bits)/8)
+
+	for i := 0; i < len(bits); i += 8 {
+		bytes[i/8] = GetByteFromBits(bits[i : i+8])
+	}
+
+	return bytes
 }
 
 // GetBitsFromBytes - Retrieves the bits of a byte
@@ -38,8 +64,8 @@ func GetBitsFromBytes(bytes []byte) []bool {
 	return bits
 }
 
-// GetBitStringFromBytes - Retrieves the bits of a byte
-// array as a string
+// GetBitStringFromBytes - Retrieves the bit string
+// of a byte array
 func GetBitStringFromBytes(bytes []byte) string {
 	var builder strings.Builder
 	bits := GetBitsFromBytes(bytes)
@@ -53,4 +79,45 @@ func GetBitStringFromBytes(bytes []byte) string {
 	}
 
 	return builder.String()
+}
+
+// GetBitStringFromString - Retrieves the decoded string
+// of a bit string
+func GetBitStringFromString(str string) string {
+	return GetBitStringFromBytes([]byte(str))
+}
+
+// GetBitsFromBitString - Converts a bit string to a
+// bit array
+func GetBitsFromBitString(bitStr string) []bool {
+	bits := make([]bool, len(bitStr))
+
+	for i, char := range bitStr {
+		if string(char) == "1" {
+			bits[i] = true
+		} else {
+			bits[i] = false
+		}
+	}
+
+	return bits
+}
+
+// GetBytesFromBitString - Converts a bit string to a
+// byte array
+func GetBytesFromBitString(bitStr string) []byte {
+	bytes := make([]byte, len(bitStr)/8)
+
+	for i := 0; i < len(bitStr); i += 8 {
+		bits := GetBitsFromBitString(bitStr[i : i+8])
+		bytes[i/8] = GetByteFromBits(bits)
+	}
+
+	return bytes
+}
+
+// GetStringFromBitString - Converts a bit string to a
+// string
+func GetStringFromBitString(bitStr string) string {
+	return string(GetBytesFromBitString(bitStr))
 }
